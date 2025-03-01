4e2dc477bf9f3c2a1d99e8dffa69bc914f0c4795
programmarimran <programmarimran@gmail.com>
2025-03-01 18:20:41 +0600

    added a matha hang?

--- js/index.js
+++ js/index.js
@@ -8,9 +8,38 @@ document.getElementById("question-btn")
 const completedBtn =document.querySelectorAll(".completed-btn  ")
 
 for (let i = 0; i < completedBtn.length; i++){
-    completedBtn[i].addEventListener("click",function(){
+    completedBtn[i].addEventListener("click",function(event){
         alert("Board Updated Succesfully");
+
         completedBtn[i].setAttribute("disabled",true);
-        
+
+        const taskAssignedNumber=convertedElementStringToNumber("task-assigned");
+        document.getElementById("task-assigned").innerText=taskAssignedNumber-1;
+
+        const navNumber=convertedElementStringToNumber("navNumber");
+        document.getElementById("navNumber").innerText=navNumber+1;
+//************************************************************ */
+const cardTitle =document.querySelectorAll(".card-title");
+
+const historyContainer =document.getElementById("history-container");
+const comment =document.createElement("div");
+const titleLengthNumber=[]
+for(let j = 0; j < cardTitle.length ; j++){
+    const title =cardTitle[j].innerText;
+    
+    const length=title.length;
+    titleLengthNumber.push(length)
+
+   
+
+for(const number of titleLengthNumber){
+
+   comment.innerHTML=`<p class="bg-themecolor rounded-lg m-6 p-3 font-normal text-base text-descriptioncolor">You have Complete The Task ${title.length==number?title:``} at 12:48:15 PM </p>`
+        console.log(number)
+}
+}
+  
+
+historyContainer.appendChild(comment);
     })
 }
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,6 @@
-alert()
+// converted innerText string to number
+function convertedElementStringToNumber (id){
+    const value=document.getElementById(id).innerText;
+    const convertedValue=parseFloat(value);
+    return convertedValue;
+}
