31d7d355d4dcfd8aa1e29ddfcc522692d3276bff
programmarimran <programmarimran@gmail.com>
2025-03-01 16:46:39 +0600

    added a utilities file create and complited button clicke then show the alert

--- js/index.js
+++ js/index.js
@@ -9,6 +9,8 @@ const completedBtn =document.querySelectorAll(".completed-btn  ")
 
 for (let i = 0; i < completedBtn.length; i++){
     completedBtn[i].addEventListener("click",function(){
-        console.log("bondo amake ki dekha jai")
+        alert("Board Updated Succesfully");
+        completedBtn[i].setAttribute("disabled",true);
+        
     })
 }
