206023ee6d74f42151c5988c660f85e0a0567a6c
programmarimran <programmarimran@gmail.com>
2025-03-02 17:57:14 +0600

    added a sompart baki

--- js/comment.js
+++ js/comment.js
@@ -2,20 +2,6 @@ for(let btn of completedBtn){
     btn.addEventListener("click",function(event){
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         const titleArry=['Review Ami Probashi Site',
             'Integrate AI search',
             'Fix Video Loading Issue',
@@ -24,42 +10,65 @@ for(let btn of completedBtn){
             'Fix Mobile Button Issue'
 
         ]
-        
-
-        
-
-
+       
         const cardTitle=document.getElementsByClassName("card-title");
+
+        let titleStore=[]
        
         for(const title of cardTitle){
+           titleStore.push(title.innerText); 
+           
+        } 
+
+        let finalTitle="";
+             for(let i of titleArry){
+
+                for(let j of titleStore){
+                    if(i==j){
+                        finalTitle+=j;
+                    }
+
+                }
+
+             }   
+             
+             const spliteFinalTitle =finalTitle.split(",")
+             const pest =spliteFinalTitle[0]
         
-            titleArry.push(title.innerText)
+       
+
+
+
+
+
+
+
+
+
+
+
+
+                    const historyContainer=document.getElementById("history-container");
+                   const dynamicComment =document.createElement("div")
+                   // dynamicComment.classList.add("bg-themecolor rounded-lg m-6 p-3 font-normal text-base text-descriptioncolor")
+                   
+                   dynamicComment.innerHTML=`
+                   <p class="dynamicComment bg-themecolor rounded-lg m-6 p-3 font-normal text-base text-descriptioncolor">
+                   You have Complete The Task ${pest} at ${time} ${timeAmOrPm}
+                   </p>
+                   `
+                   historyContainer.appendChild(dynamicComment)
+                 
+
+
+                
 
 
         
-
-
-        const historyContainer=document.getElementById("history-container");
-
-        const dynamicComment =document.createElement("div")
-        // dynamicComment.classList.add("bg-themecolor rounded-lg m-6 p-3 font-normal text-base text-descriptioncolor")
-        
-        dynamicComment.innerHTML=`
-        <p class="dynamicComment bg-themecolor rounded-lg m-6 p-3 font-normal text-base text-descriptioncolor">
-        You have Complete The Task ${title.innerText} at ${time} ${timeAmOrPm}
-        </p>
-        `
-
     
-        historyContainer.appendChild(dynamicComment)
     
-        
-    }
+    
     
    
-
-
-
-
     })
 }
--- js/utilities.js
+++ js/utilities.js
@@ -7,24 +7,3 @@ function convertedElementStringToNumber (id){
 
 //************************************** */
 
-// No 1=>getElementById
-// No 2=>getElementsByClassName
-// No 3=>querySelector
-// No 4=>querySelectorAll
-// No 5=>getElementsByTagName
-
-let element = document.getElementById("question-btn");
-console.log(element.textContent); // Logs the text inside the div
-// element.textContent = "New text content"; // Updates the text
-//**
-// innerHt
-document.getElementById("idhfgg").setAttribute
-document.getElementById("idhfgg").removeAttribute
-document.getElementById("idhfgg").removeAttribute
-
-// let sdfd=
-// let dfgh.
-
-// Create: document.createElement(),
-// Remove: document.removeChild(),
-//  and then push the container.appendChild()
