fa8b434e9eff7c97ab5329285f7591138fb8ae95
programmarimran <programmarimran@gmail.com>
2025-03-02 16:28:28 +0600

    added  the complite questons Ans:

--- js/utilities.js
+++ js/utilities.js
@@ -1,4 +1,4 @@
-// converted innerText string to number
+// converted textContent string to number
 function convertedElementStringToNumber (id){
     const value=document.getElementById(id).innerText;
     const convertedValue=parseFloat(value);
@@ -7,3 +7,24 @@ function convertedElementStringToNumber (id){
 
 //************************************** */
 
+// No 1=>getElementById
+// No 2=>getElementsByClassName
+// No 3=>querySelector
+// No 4=>querySelectorAll
+// No 5=>getElementsByTagName
+
+let element = document.getElementById("question-btn");
+console.log(element.textContent); // Logs the text inside the div
+// element.textContent = "New text content"; // Updates the text
+//**
+// innerHt
+document.getElementById("idhfgg").setAttribute
+document.getElementById("idhfgg").removeAttribute
+document.getElementById("idhfgg").removeAttribute
+
+// let sdfd=
+// let dfgh.
+
+// Create: document.createElement(),
+// Remove: document.removeChild(),
+//  and then push the container.appendChild()
